c824237060dce2dc96465583c72b123626863c97
Shubham Sahu <shubham@rocketium.com>
2023-12-09 17:57:21 +0530

    chore: server init

--- src/UploadDropZone.tsx
+++ src/UploadDropZone.tsx
@@ -30,12 +30,8 @@ const UploadDropZone = () => (
 		<p className="ant-upload-drag-icon">
 			<InboxOutlined />
 		</p>
-		<p className="ant-upload-text">
-			Click or drag file to this area to upload
-		</p>
-		<p className="ant-upload-hint">
-			Support for a single or bulk upload.
-		</p>
+		<p className="ant-upload-text">Click or drag file to this area to upload</p>
+		<p className="ant-upload-hint">Support for a single or bulk upload.</p>
 	</Dragger>
 );
 
