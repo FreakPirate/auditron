63e4f47269a1aead50193260f512fc3c48a95d63
Shubham Sahu <shubham@rocketium.com>
2023-12-09 18:45:35 +0530

    feat: api routes for firebase

--- server/firestore/adapter.ts
+++ server/firestore/adapter.ts
@@ -11,16 +11,14 @@ import {
 	AuditFile,
 } from './types';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyBz2niEiigVe39gL3dAFjQRqEHLlb4esiY',
-	authDomain: 'auditron-eth.firebaseapp.com',
-	projectId: 'auditron-eth',
-	storageBucket: 'auditron-eth.appspot.com',
-	messagingSenderId: '82588898126',
-	appId: '1:82588898126:web:aef6653795570d4bacc41f',
-	measurementId: 'G-PRWF9EBB3D',
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import multer from 'multer';
 import fs from 'fs';
 import pinataSDK from '@pinata/sdk';
+import { UserRole } from './firestore/types';
+import {
+	assignAuditorToProject,
+	createNewAuditFile,
+	createNewBid,
+	createNewProject,
+	createNewUser,
+	getActiveBidProjectsForStakeholder,
+	getActiveProjects,
+	getAvailableBidProjectsForAuditor,
+	getBidsForProject,
+	getCompletedProjects,
+	updateProjectAiAuditStatus,
+	updateProjectAuditorId,
+	updateProjectManualAuditStatus,
+	updateProjectStatus,
+} from './firestore/adapter';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -13,7 +30,7 @@ app.get('/', (req, res) => {
 	res.send('Hello, TypeScript with Express!');
 });
 
-app.post('/ipfs/upload', upload.single('file'), async (req, res) => {
+app.post('/file/upload', upload.single('file'), async (req, res) => {
 	console.log('received file to be uploaded on IPFS => ', req.file?.path, req.body);
 
 	if (!req.file) {
@@ -31,6 +48,180 @@ app.post('/ipfs/upload', upload.single('file'), async (req, res) => {
 	});
 	console.log('Response from Pinata => ', pinataRes);
 	return res.status(200).json({ ...pinataRes, url: `https://gateway.pinata.cloud/ipfs/${pinataRes.IpfsHash}` });
+});
+
+app.get('/api/active-projects/:userId/:role', async (req: Request, res: Response) => {
+	try {
+		const userId = req.params.userId as string;
+		const role = req.params.role as UserRole;
+		const projects = await getActiveProjects(userId, role);
+		res.status(200).json({
+			data: projects,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.get('/api/completed-projects/:userId/:role', async (req: Request, res: Response) => {
+	try {
+		const userId = req.params.userId as string;
+		const role = req.params.role as UserRole;
+		const projects = await getCompletedProjects(userId, role);
+		res.status(200).json({
+			data: projects,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.get('/api/active-bid-projects/stakeholder/:stakeholderId', async (req: Request, res: Response) => {
+	try {
+		const stakeholderId = req.params.stakeholderId as string;
+		const projects = await getActiveBidProjectsForStakeholder(stakeholderId);
+		res.status(200).json({
+			data: projects,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.get('/api/bids/project/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		const bids = await getBidsForProject(projectId);
+		res.status(200).json({
+			data: bids,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.get('/api/available-bid-projects/auditor/:auditorId', async (req: Request, res: Response) => {
+	try {
+		const auditorId = req.params.auditorId as string;
+		const projects = await getAvailableBidProjectsForAuditor(auditorId);
+		res.status(200).json({
+			data: projects,
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.post('/api/project', async (req: Request, res: Response) => {
+	try {
+		const newProject = req.body;
+		await createNewProject(newProject);
+		res.status(200).json({
+			message: 'Project created successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.post('/api/bid', async (req: Request, res: Response) => {
+	try {
+		const newBid = req.body;
+		await createNewBid(newBid);
+		res.status(200).json({
+			message: 'Bid created successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.patch('/api/project/status/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		const status = req.body.status;
+		updateProjectStatus(projectId, status);
+		res.status(200).json({
+			message: 'Project status updated successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.patch('/api/project/manual-audit-status/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		const status = req.body.status;
+		updateProjectManualAuditStatus(projectId, status);
+		res.status(200).json({
+			message: 'Project manual audit status updated successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.patch('/api/project/ai-audit-status/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		const status = req.body.status;
+		updateProjectAiAuditStatus(projectId, status);
+		res.status(200).json({
+			message: 'Project AI audit status updated successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.patch('/api/project/auditor/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		const auditorId = req.body.auditorId;
+		updateProjectAuditorId(projectId, auditorId);
+		res.status(200).json({
+			message: 'Project auditor ID updated successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.post('/api/user', async (req: Request, res: Response) => {
+	try {
+		const newUser = req.body;
+		await createNewUser(newUser);
+		res.status(200).json({
+			message: 'User created successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.post('/api/audit-file', async (req: Request, res: Response) => {
+	try {
+		const newAuditFile = req.body;
+		await createNewAuditFile(newAuditFile);
+		res.status(200).json({
+			message: 'Audit file created successfully',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
+});
+
+app.patch('/api/project/assign-auditor/:projectId', async (req: Request, res: Response) => {
+	try {
+		const projectId = req.params.projectId as string;
+		await assignAuditorToProject(projectId);
+		res.status(200).json({
+			message: 'Auditor assigned successfully!',
+		});
+	} catch (error) {
+		res.status(500).json({ message: error });
+	}
 });
 
 app.listen(port, () => {
